ebd5dd23d820fbad6df2ecef6889d3a834da7710
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2023-08-26 14:05:29 -0700

    Update NavBar.js
    Code Refactoring

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,6 @@
 import { useEffect, useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
+import {AppBar, Box, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography, Button}  from "@mui/material"
 import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import { useLocation, useNavigate } from "react-router-dom";
 
 const drawerWidth = 240;
