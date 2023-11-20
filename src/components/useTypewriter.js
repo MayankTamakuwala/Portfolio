1203769c1bac212599c70ea23c4adb8c926fb4a6
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2023-11-20 11:35:22 -0800

    Update useTypewriter.js

--- src/components/useTypewriter.js
+++ src/components/useTypewriter.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 
-function useTypewriter( words, deleteDelay = 150, typeDelay = 300) {
+function useTypewriter( words, deleteDelay = 50, typeDelay = 75) {
     const [wordIndex, setWordIndex] = useState(0);
     const [text, setText] = useState("");
     const [isDeleting, setIsDeleting] = useState(false);
