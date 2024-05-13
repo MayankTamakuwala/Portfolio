bb4a75fdb2bcc9fd41101c18be40d67bc19ed31d
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2024-05-13 01:28:14 -0400

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -175,7 +175,7 @@ const Form = () => {
                             id="about"
                             name="about"
                             rows={3}
-                            className="block bg-transparent w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+                            className="block bg-transparent w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
                         />
                     </div>
                     <p className="mt-3 text-sm leading-6 text-gray-300">Write whatever's on your mind.</p>
