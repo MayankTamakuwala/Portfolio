165d3973c91ec328b27f307e3acb8682a4ca430d
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2024-05-21 00:11:48 -0400

    Resume and About Text Update

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -9,15 +9,21 @@ const Resume = () => {
                 width:"100vw",
                 scrollbarWidth:"none"
             }}
-            className="lg:mt-10 mt-11"
+            className="lg:mt-10 mt-11 overflow-x-hidden"
         >
-            <object
-                title="Resume"
-                type="application/pdf"
-                style={{ background: "white", scrollbarWidth: "none" }}
-                data={require("../assets/resume/Mayank_Tamakuwala_Resume.pdf")}
-                height="99.9%"
-                width="100%"
+            {/* // <object
+            //     title="Resume"
+            //     type="application/pdf"
+            //     style={{ background: "white", scrollbarWidth: "none" }}
+            //     data={require("../assets/resume/Mayank_Tamakuwala_Resume.pdf")}
+            //     height="99.9%"
+            //     width="100%"
+            // /> */}
+            <iframe 
+                src={require("../assets/resume/Mayank_Tamakuwala_Resume.pdf")}
+                width={"100%"}
+                height={"99.9%"}
+                title="Mayank Tamakuwala Resume"
             />
         </div>
     )
